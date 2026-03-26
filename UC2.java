34892019dfad7e11d4fbddc347b046fdc5cd7824
vinaynair1207 <Vinaynair1207@gmail.com>
2026-03-26 10:09:32 +0530

    UC2 Added

---
+++ UC2.java
@@ -0,0 +1,11 @@
+package HELLOAPP;
+import java.util.*;
+public class UC2 {
+    public static void main(String[]args)
+    {
+        Scanner a=new Scanner(System.in);
+        System.out.print("Enter name:");
+        String b=a.nextLine();
+        System.out.println("Hello"+" "+b);
+    }
+}
